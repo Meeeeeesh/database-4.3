c6e0608240d721c68783722e907672286ebd72a9
Michelle Hood <meesh121@gmail.com>
2015-07-09 10:28:03 -0400

    updated in class

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,9 +1,5 @@
 class Album < ActiveRecord::Base
   validates :title, :genre, :artist, :length, presence: true
-  validates :length, numerically: { greater_than: 0 }
-  validates :length, numerically: true
-  validates :length, numerically: { only_integer: true }
-  validates :length, length: ( minimum: 1 )
-  validates_uniqueness_of :title { scope: :artist }
-  validates_length_of :title, length: ( minimum: 1 )
+  validates :length, numericality: { greater_than: 0, only_integer: true }
+  validates :title, uniqueness: { scope: :artist }
 end
--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,100 +1,77 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
 
-  # test "entry invalid without a title" do
-  #   a = Album.new
-  #   refute a.vailid?
-  # end
- 
   test "duplicate entries" do
-    a = Album.new(artist: 'The Postal Service', genre: 'Electronic', title: 'Nothing Better', length: 347)
-    a.save
+    Album.create!(artist: 'The Postal Service', genre: 'Electronic', title: 'Nothing Better', length: 347)
     z = Album.new(artist: 'The Postal Service', genre: 'Electronic', title: 'Nothing Better', length: 347)
     refute z.valid?
   end
  
-  test "multiple entries by same artist"
-    b = Album.new(artist: 'The Postal Service', genre: 'Electronic', title: 'Nothing Better', length: 347)
-    a.save
+  test "can create multiple entries by same artist" do
+    Album.create!(artist: 'The Postal Service', genre: 'Electronic', title: 'Nothing Better', length: 347)
     y = Album.new(artist: 'The Postal Service', genre: 'Electronic', title: 'Brand New Colony', length: 415)
     assert y.valid?
   end
  
   test "validates can be saved and sourced" do
-    a = Album.new(artist: 'The Postal Service', genre: 'Electronic', title: 'Nothing Better', length: 347)
-    a.save
+    Album.create!(artist: 'The Postal Service', genre: 'Electronic', title: 'Nothing Better', length: 347)
     source = Album.find_by(title: 'Nothing Better')
-    assert source
+    assert source.present?
   end
  
   test "should have necessary required validators" do
     a = Album.new
-    assert_not a.valid?
-    asser_equal [:title, :artist, :genre, :length], a.errors.keys
+    refute a.valid?
+    [:title, :artist, :genre, :length].each do |key|
+      assert a.errors.keys.include?(key)
+    end
   end
  
   test "invalid without artist" do 
-    a = Album.new(title: 'Nothing Better', genre: 'Electronic', length: 347)
-    asser_equal a.vailid?, false 
+    a = Album.new(artist: '', title: 'Nothing Better', genre: 'Electronic', length: 347)
+    refute a.valid? 
   end
  
   test "invalid without title" do 
-    a = Album.new(artist: 'The Postal Service', genre: 'Electronic', length: 347)
-    asser_equal a.vailid?, false 
+    a = Album.new(artist: 'The Postal Service', title: '', genre: 'Electronic', length: 347)
+    refute a.valid? 
   end
  
   test "invalid without genre" do 
-    a = Album.new(artist: 'The Postal Service', title: 'Nothing Better', length: 347)
-    asser_equal a.vailid?, false 
+    a = Album.new(artist: 'The Postal Service', title: 'Nothing Better', genre: '', length: 347)
+    refute a.valid? 
   end
  
   test "invalid without length" do 
-    a = Album.new(artist: 'The Postal Service', genre: 'Electronic', title: 'Nothing Better')
-    asser_equal a.vailid?, false 
+    a = Album.new(artist: 'The Postal Service', genre: 'Electronic', title: 'Nothing Better', length: '')
+    refute a.valid?  
   end
 
   test "length should be numeric" do
     a = Album.new(title: 'Nothing Better', genre: 'Electronic', artist: 123, length: 'string')
-    asser_equal a.vailid?, false 
-  end
-
-  test "title should NOT be numeric" do
-    a = Album.new(title: 123, genre: 'Electronic', artist: 'The Postal Service', length: 347)
-    asser_equal a.vailid?, false 
-  end
-
-  test "genre should NOT be numeric" do
-    a = Album.new(title: 'Nothing Better', genre: 123, artist: 'The Postal Service', length: 347)
-    asser_equal a.vailid?, false 
-  end
- 
-  test "artist should NOT be numeric" do
-    a = Album.new(title: 'Nothing Better', genre: 'Electronic', artist: 123, length: 347)
-    asser_equal a.vailid?, false 
+    refute a.valid?  
   end
   
   test "length should NOT be blank" do
     a = Album.new(title: 'Nothing Better', genre: 'Electronic', artist: 'The Postal Service', length: '')
-    asser_equal a.vailid?, false 
+    refute a.valid?
+    assert a.errors.keys.include?(:length), 'What is this?!'
   end
   
   test "title should NOT be blank" do
     a = Album.new(title: '', genre: 'Electronic', artist: 'The Postal Service', length: 347)
-    asser_equal a.vailid?, false 
+    refute a.valid?  
   end
  
   test "genre should NOT be blank" do
     a = Album.new(title: 'Nothing Better', genre: '', artist: 'The Postal Service', length: 347)
-    asser_equal a.vailid?, false 
+    refute a.valid?  
   end
  
   test "artist should NOT be blank" do
     a = Album.new(title: 'Nothing Better', genre: 'Electronic', artist: '', length: 347)
-    asser_equal a.vailid?, false 
+    refute a.valid?  
   end
   
 end
